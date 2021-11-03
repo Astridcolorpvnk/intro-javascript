0d80fd5b0937c72ff32d89401a7d9fcac92b7799
Astridcolorpvnk <redrumpanic@gmail.com>
2021-11-02 22:31:33 -0300

    closuress

--- 5_closures/5_module.js
+++ 5_closures/5_module.js
@@ -4,19 +4,21 @@ var counter = (function(firstValue){
     var currentValue = firstValue
 
     var logger = function () {
-
+console.log("El valor actual es: ",currentValue);
     }
     var increment = function(step){
-
+        currentValue += step
+        logger()
     }
     var decrement = function(step){
-
+        currentValue -= step
+        logger()
 
     }
 
     return { inc: increment,
             dec: decrement }
-})()
+})(0)
 counter.inc(2)
 counter.dec(3)
 
